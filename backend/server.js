27e49fe6b989a6601becce9bf22c32a9ff140d4a
KARTHIK  PARISHAPOGU <110421139+KarthikParishapogu0511@users.noreply.github.com>
2026-08-05 11:53:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,10 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const allowedOrigins = new Set([
+const allowedOrigins = [
   'http://localhost:5173','https://medical-devices-demand-inventory-procurement-optimis-mf65117ue.vercel.app/',
   process.env.FRONTEND_URL
-]);
+].filter(Boolean);
 
 app.use(cors({
   origin(origin, callback) {
