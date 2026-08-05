61cee3a77204b732ca4396868c763e3df7c9efe7
KARTHIK  PARISHAPOGU <110421139+KarthikParishapogu0511@users.noreply.github.com>
2026-08-05 12:01:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 const allowedOrigins = [
-  'http://localhost:5173','https://medical-devices-demand-inventory-procurement-optimis-mf65117ue.vercel.app/',
+  'http://localhost:5173','https://medical-devices-demand-inventory-procurement-optimis-mf65117ue.vercel.app',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
